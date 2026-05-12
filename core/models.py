bafb937e69a79a94c2f60fb1481da06ac84fa22a
chandrarenu <chandrarenu07@gmail.com>
2026-05-12 15:36:25 +0545

    add staff model and admin panel

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Staff(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    address = models.TextField()
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return self.name
